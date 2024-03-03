8bf7aee8f05bde7e474b79957862ef6ddbb85049
Nickk91 <nickkaploon@gmail.com>
2024-03-03 14:36:37 +0200

    added a router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import Counter from "./components/Counter/Counter.jsx";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import LoggedOutPage from "./pages/LoggedOutPage/LoggedOutPage.jsx";
 function App() {
   return (
     <>
-      <h1>THIS IS DEVELOPMENT SERVER</h1>
-      <Counter />;
+      <h1>{import.meta.env.VITE_BASEURL}</h1>
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/loggedout" exact element={<LoggedOutPage />} />
+
+        {/* <Counter />; */}
+      </Routes>
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
