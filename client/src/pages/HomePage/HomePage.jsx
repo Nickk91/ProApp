0f90aee7ac52428a14a7763005652f41db744fa5
Nickk91 <nickkaploon@gmail.com>
2024-03-18 22:05:49 +0200

    created the homepage with the no projects view and the add projects icon

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "../style/pagestyle.css";
 import * as S from "./styles.js";
-import heroImg from "../../assets/images/NoProjects.png";
+import heroImg from "../../assets/images/NoProjects.svg";
+import addLogo from "../../assets/images/icon _Plus_Circle_.svg";
 
 const HomePage = () => {
-  return <section className="page">
+  return (
+    <section className="page">
       <S.formTitle>My Projects</S.formTitle>
       <S.hero src={heroImg} alt="Hero Image of no projects" />
-
-  </section>
+      <S.h2>Add your first project</S.h2>
+      <S.addLogo src={addLogo} alt="Hero Image of no projects" />
+    </section>
+  );
 };
 
 export default HomePage;
