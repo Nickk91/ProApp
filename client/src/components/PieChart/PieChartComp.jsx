3dc977bc9cb14f47f104f3933e129823ef0ae4e6
Nickk91 <nickkaploon@gmail.com>
2024-06-08 15:59:45 +0300

    added charts and refactored code

--- client/src/components/PieChart/PieChartComp.jsx
+++ client/src/components/PieChart/PieChartComp.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { PieChart, Pie, Tooltip } from "recharts";
+import * as S from "./styled.js";
 
-const PieChartComp = ({ data, fill }) => {
+const PieChartComp = ({ data, fill, title }) => {
   return (
-    <PieChart width={400} height={400}>
-      <Pie
-        dataKey="value"
-        isAnimationActive={true}
-        data={data}
-        cx={200}
-        cy={200}
-        outerRadius={80}
-        fill={fill}
-        label
-      />
-      <Tooltip />
-    </PieChart>
+    <>
+      {/* <div>{title}</div> */}
+      <S.chartContainer>
+        <PieChart width={400} height={400}>
+          <Pie
+            dataKey="value"
+            isAnimationActive={true}
+            data={data}
+            cx={200}
+            cy={200}
+            outerRadius={80}
+            fill={fill}
+            label={(entry) => entry.label}
+          />
+          <Tooltip />
+        </PieChart>
+      </S.chartContainer>
+    </>
   );
 };
 
---
+++ client/src/components/PieChart/styled.js
@@ -0,0 +1,11 @@
+//styled.js
+
+import styled from "styled-components";
+
+const chartContainer = styled.div`
+  width: 350px;
+  height: 350px;
+  margin-top: -120px;
+`;
+
+export { chartContainer };
