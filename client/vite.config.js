498eef54123caf379d28dd2c9dc1ca11dfea8cca
Nickk91 <nickkaploon@gmail.com>
2024-10-12 17:05:53 +0300

    added parcel, babel, polyfilling to improve compitability with older browsers

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    legacy({
+      targets: ["defaults", "not IE 11", "safari >= 10"], // Define the browser targets
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        // Custom manual chunking
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            // Bundle all node_modules dependencies into one 'vendor' chunk
+            return "vendor";
+          }
+        },
+      },
+    },
+    // Optionally, adjust the warning limit for large chunks
+    chunkSizeWarningLimit: 1000, // Increase limit to 1000 KB to avoid warnings for larger chunks
+  },
+});
