f9d41946ad02039ae416f7197b8e6388b7aaeec7
Nickk91 <nickkaploon@gmail.com>
2025-01-11 16:20:50 +0200

    Fixed the position of the spinner to be centered on the userpage

--- client/src/pages/UserPage/Userpage.jsx
+++ client/src/pages/UserPage/Userpage.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from "react";
 import * as S from "./styles.js";
 import Spinner from "../../components/Spinner/Spinner.jsx";
 import FooterMenu from "../../components/FooterMenu/FooterMenu.jsx";
-import "../style/pagestyle.css";
 import { useDispatch, useSelector } from "react-redux";
 import { userAuthLevels } from "../../constants/userAuthLevels.js";
 import { useParams, useNavigate } from "react-router-dom";
@@ -13,93 +12,80 @@ import ImageModal from "../../components/ImageModal/ImageModal.jsx";
 import addTask from "../../assets/images/icon_Plus_Circle_.svg";
 import { setUserId } from "../../slices/userIdSlice.js";
 import * as ST from "../../components/StyledComponents/styles.jsx";
+import "../style/pagestyle.css";
 
 const Userpage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [userData, setUserData] = useState(null);
   const [projects, setProjects] = useState([]);
-  const [userIdToSearchBy, setUserIdToSearchBy] = useState();
-  const [isProjectsLoading, setIsProjectsLoading] = useState(true);
   const [imageModalOpen, setImageModalOpen] = useState(false);
-  const [avatarUpdated, setAvatarUpdated] = useState(false);
   const [imageError, setImageError] = useState(null);
-  const formRef = useRef(null);
-  const [isImageLoaded, setIsImageLoaded] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(null);
 
+  const formRef = useRef(null);
   const { userId } = useParams();
   const authLevel = useSelector((state) => state.auth.user?.authLevel);
-  const id = useSelector((state) => state.auth.user._id);
+  const currentUserId = useSelector((state) => state.auth.user._id);
   const token = localStorage.getItem("token");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchUser = async (url) => {
+    const fetchData = async () => {
       try {
         if (!token) throw new Error("No token found. Please log in");
 
-        const response = await fetch(url, {
+        const userUrl =
+          authLevel === userAuthLevels.admin && userId !== currentUserId
+            ? `${import.meta.env.VITE_BASEURL}/users/${userId}`
+            : `${import.meta.env.VITE_BASEURL}/users/current`;
+
+        const userResponse = await fetch(userUrl, {
           method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
+          headers: { Authorization: `Bearer ${token}` },
         });
 
-        if (!response.ok) throw new Error("Failed to fetch user");
+        if (!userResponse.ok) throw new Error("Failed to fetch user");
 
-        const data = await response.json();
-        setUserData(data);
-        setUserIdToSearchBy(data._id);
+        const userData = await userResponse.json();
+        setUserData(userData);
+
+        const projectsResponse = await fetch(
+          `${import.meta.env.VITE_BASEURL}/projects/project/user/${
+            userData._id
+          }`,
+          {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+
+        if (!projectsResponse.ok) throw new Error("Failed to fetch projects");
+
+        const projectsData = await projectsResponse.json();
+        setProjects(projectsData.projects);
       } catch (error) {
-        setErrorMessage(error.message);
+        setImageError(error.message);
         console.error(error);
       } finally {
         setIsLoading(false);
       }
     };
 
-    const url =
-      authLevel === userAuthLevels.admin && userId !== id
-        ? `${import.meta.env.VITE_BASEURL}/users/${userId}`
-        : `${import.meta.env.VITE_BASEURL}/users/current`;
+    fetchData();
+  }, [authLevel, userId, currentUserId, token]);
 
-    fetchUser(url);
-  }, [authLevel, userId, id, token, avatarUpdated]);
-
-  useEffect(() => {
-    const fetchProjects = async () => {
-      if (!userIdToSearchBy) {
-        return;
-      }
-      setIsProjectsLoading(true);
-      try {
-        if (!token) throw new Error("No token found");
-
-        const response = await fetch(
-          `${
-            import.meta.env.VITE_BASEURL
-          }/projects/project/user/${userIdToSearchBy}`,
-          {
-            method: "GET",
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-
-        if (!response.ok) throw new Error("Failed to fetch projects");
-
-        const data = await response.json();
-        setProjects(data.projects);
-      } catch (error) {
-        console.error("Error fetching projects:", error);
-        setErrorMessage(error.message);
-      } finally {
-        setIsProjectsLoading(false);
-      }
-    };
-
-    fetchProjects();
-  }, [userIdToSearchBy, token]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(formRef.current);
+    const url = formData.get("url");
+    if (checkIfUrl(url)) {
+      changeUserPic(userData._id, url);
+      setImageModalOpen(false);
+      setImageError(null);
+    } else {
+      setImageError("INVALID URL!");
+    }
+  };
 
   const changeUserPic = async (userId, url) => {
     try {
@@ -119,92 +105,61 @@ const Userpage = () => {
 
       if (!response.ok) throw new Error("Failed to update user picture");
 
-      setAvatarUpdated((prev) => !prev);
+      setUserData((prev) => ({ ...prev, avatar: url }));
     } catch (error) {
       console.error("Error updating user picture:", error);
       setImageError(`Error updating user picture: ${error.message}`);
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(formRef.current);
-    const url = formData.get("url");
-    if (checkIfUrl(url)) {
-      changeUserPic(userId, url);
-      setImageModalOpen(false);
-      setImageError(null);
-    } else {
-      setImageError("INVALID URL!");
-    }
-  };
-
   const projectsCounts = getProjectCounts(projects);
-
   const taskCounts = getTaskCounts(projects);
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleAddProject = (user_id) => {
-    dispatch(setUserId(user_id));
-    navigate("/addproject");
-  };
-
-  const handleImageLoad = () => {
-    setIsImageLoaded(true);
-  };
 
   return (
     <section className="page">
-      <S.userTitle>User Page:</S.userTitle>
       {isLoading ? (
         <Spinner />
-      ) : errorMessage ? (
-        <ST.ErrorBox>{errorMessage}</ST.ErrorBox>
       ) : (
         <>
-          {!isImageLoaded && <S.ImagePlaceholder />}
-          <S.UserImgWrapper data-tooltip="Click to replace image!">
-            <S.UserImg
-              src={userData.avatar}
-              alt={`${userData.username}'s avatar`}
-              onClick={() => setImageModalOpen((prev) => !prev)}
-              onLoad={handleImageLoad}
-              style={{ display: isImageLoaded ? "block" : "none" }}
-            />
-          </S.UserImgWrapper>
-          <S.container>
-            <S.list>
-              <S.userDetailsContainer>
-                <S.h3>User Details:</S.h3>
-                <S.listItem>Username: {userData?.username}</S.listItem>
-                <S.listItem>User Email: {userData?.email}</S.listItem>
-                <S.listItem>User Id: {userData?._id}</S.listItem>
-              </S.userDetailsContainer>
+          {imageError ? (
+            <ST.ErrorBox>{imageError}</ST.ErrorBox>
+          ) : (
+            <>
+              <S.userTitle>User Page:</S.userTitle>
+              <S.UserImgWrapper data-tooltip="Click to replace image!">
+                <S.UserImg
+                  src={userData.avatar}
+                  alt={`${userData.username}'s avatar`}
+                  onClick={() => setImageModalOpen((prev) => !prev)}
+                />
+              </S.UserImgWrapper>
+              <S.container>
+                <S.list>
+                  <S.userDetailsContainer>
+                    <S.h3>User Details:</S.h3>
+                    <S.listItem>Username: {userData?.username}</S.listItem>
+                    <S.listItem>User Email: {userData?.email}</S.listItem>
+                    <S.listItem>User Id: {userData?._id}</S.listItem>
+                  </S.userDetailsContainer>
 
-              {isProjectsLoading ? (
-                <Spinner />
-              ) : (
-                <>
                   <S.actionsContainer>
                     <S.addProjectContainer>
                       <h3>Add Project:</h3>
-
                       <S.addProjectIcon
                         src={addTask}
                         onClick={() => {
-                          handleAddProject(userData._id);
+                          dispatch(setUserId(userData._id));
+                          navigate("/addproject");
                         }}
                       />
                     </S.addProjectContainer>
-                    {projects.length > 0 ? (
+                    {projects.length > 0 && (
                       <S.userPorjectsBtn
                         onClick={() => navigate(`/projects-of-user/${userId}`)}
                       >
                         User's Projects
                       </S.userPorjectsBtn>
-                    ) : null}
+                    )}
                   </S.actionsContainer>
 
                   <S.chartsContainer>
@@ -231,37 +186,31 @@ const Userpage = () => {
                         )}
                       </>
                     ) : (
-                      <>
-                        <h3>
-                          <i>No projects found</i>
-                        </h3>
-                      </>
+                      <h3>
+                        <i>No projects found</i>
+                      </h3>
                     )}
                   </S.chartsContainer>
-                </>
-              )}
-            </S.list>
-          </S.container>
-          <FooterMenu />
+                </S.list>
+              </S.container>
+              <FooterMenu />
+            </>
+          )}
+          <ImageModal
+            toDelete="project"
+            isOpen={imageModalOpen}
+            onRequestClose={() => setImageModalOpen(false)}
+            onRequestChangeUrl={handleSubmit}
+          >
+            <form ref={formRef} onSubmit={handleSubmit}>
+              Please enter the URL of your image
+              <S.urlInput type="url" name="url" required />
+              <S.acceptBtn type="submit">Accept</S.acceptBtn>
+            </form>
+            {imageError && <S.errorMessage>{imageError}</S.errorMessage>}
+          </ImageModal>
         </>
       )}
-      <ImageModal
-        toDelete="project"
-        isOpen={imageModalOpen}
-        onRequestClose={() => setImageModalOpen(false)}
-        onRequestChangeUrl={handleSubmit}
-      >
-        <form ref={formRef} onSubmit={handleSubmit}>
-          Please enter the URL of your image
-          <S.urlInput type="url" name="url" required />
-          <S.acceptBtn type="submit">Accept</S.acceptBtn>
-        </form>
-        {imageError ? (
-          <S.errorMessage>{imageError}</S.errorMessage>
-        ) : (
-          <S.errorMessageHidden />
-        )}
-      </ImageModal>
     </section>
   );
 };
